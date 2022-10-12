112c3bce196f91bd7e4b0548832018908afc354f
Logan Thurman <thurmanloganblake@gmail.com>
2022-10-12 03:39:46 +0000

    create javascript functionality

---
+++ script.js
@@ -0,0 +1,51 @@
+var question;
+var answer;
+var isQuestion;
+var punchDelivered = true;
+
+document.getElementById("button").addEventListener("click", function showJoke(){
+   
+   if(punchDelivered){
+       document.getElementById("question").innerHTML = "";
+       document.getElementById("punchLine").innerHTML = "";
+       
+       var url = "https://icanhazdadjoke.com";
+       fetch('https://icanhazdadjoke.com/', {
+        headers: {
+            'Accept': 'application/json'
+        }
+        }).then(function(response){
+            return response.json();
+            }).then(function(json){
+            console.log(json.joke);
+            let joke = json.joke;
+            isQuestion = joke.includes('?');
+            let halves = joke.split(/[?.]+/);
+            console.log(halves);
+            if(isQuestion){
+                halves[0] += '?';
+            }
+            else{
+                halves[0] += ".";
+            }
+            let hasAnswer = halves[halves.length - 1] != "" || halves.length > 2;
+            question = halves[0];
+            answer = halves.slice(1).join("");
+            
+            document.getElementById("question").innerHTML = question;
+            if(hasAnswer){
+                punchDelivered = false;
+                document.getElementById("button").innerHTML = "what?";
+            }
+            
+            
+            });
+   }
+   else{
+       document.getElementById("punchLine").innerHTML = answer;
+       punchDelivered = true;
+       document.getElementById("button").innerHTML = "New Joke";
+   }
+});
+
+
